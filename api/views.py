b21456c3b43ecb301a4812d0a3052303d6c1f816
Mehrangerami <gerami.job@gmail.com>
2023-12-15 19:37:29 +0330

    ok persmission 1

--- api/views.py
+++ api/views.py
@@ -3,20 +3,25 @@ from blog.models import Article
 from .seriallizers import ArticleSerializer,UserSerializer
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from django.contrib.auth.models import User
-
+from rest_framework.permissions import IsAdminUser
 
 class ArticleList(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    
+
 
 class ArticleDetail(RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    
 
 class UserList(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
 
 class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
