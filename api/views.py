525274803e7177a601b8243ac167bbbda0204f4c
Mehran Gerami Asl <gerami.job@gmail.com>
2023-12-18 20:43:11 +0330

    permission 2

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from .seriallizers import ArticleSerializer,UserSerializer
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAdminUser
-from .permissiones import IsSuperUser
+from .permissions import IsSuperUser,IsAuthorOrReadOnly,IsStaffOrReadOnly,IsSuperUserOrStaffReadOnly
 
 class ArticleList(ListCreateAPIView):
     queryset = Article.objects.all()
@@ -15,14 +15,16 @@ class ArticleList(ListCreateAPIView):
 class ArticleDetail(RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = (IsAuthorOrReadOnly,IsStaffOrReadOnly)
     
 
 class UserList(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsSuperUser,)
+    permission_classes = (IsSuperUserOrStaffReadOnly,)
 
 class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsSuperUser,)
+    permission_classes = (IsSuperUserOrStaffReadOnly,)
+    
--- config/settings.py
+++ config/settings.py
@@ -129,6 +129,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'api.permissions.IsStaffOrReadOnly',
+    ]
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+      #  'rest_framework.authentication.SessionAuthentication',
     ]
 }
